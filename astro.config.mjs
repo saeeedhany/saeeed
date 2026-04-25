1515e21eaa46456300ac1235956c37f7243c8dc2
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-04-25 13:13:03 +0300

    fix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ysm.dev',
+  site: 'https://saeeed.vercel.app',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {

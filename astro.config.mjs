b66dd4fce24eec36fe4ab901a658e0508c769092
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-04-25 13:20:13 +0300

    hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://saeeed.vercel.app',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
